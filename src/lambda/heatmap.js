4900272cd7ef99a67fc27c865b84bcdb3b56284b
Selcuk Cihan <selcukcihan@gmail.com>
2019-11-27 11:38:33 +0300

    Fix cors origin

--- src/lambda/heatmap.js
+++ src/lambda/heatmap.js
@@ -7,7 +7,7 @@ module.exports.handler = async (event, context) => {
             statusCode: 200,
             body: JSON.stringify(res),
             headers: {
-                "Access-Control-Allow-Origin": "selcukcihan.com",
+                "Access-Control-Allow-Origin": "https://twheat-map.selcukcihan.com",
                 "Access-Control-Allow-Credentials": true
             }
         };
